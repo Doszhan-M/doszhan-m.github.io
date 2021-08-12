26f86f5dda4e8d894ebb95629cd4f9a3ba50e3c2
Doszhan <doszhan@mail.com>
2021-08-12 22:09:29 +0600

    add abaout section

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // Фиксация navbar после скрола
-jQuery(window).scroll(function() { 
+jQuery(window).scroll(function navFixed() { 
     var the_top = jQuery(document).scrollTop();
     if (the_top > 600) {
         jQuery('.header').addClass('fixed');
@@ -7,4 +7,31 @@ jQuery(window).scroll(function() {
     else {
         jQuery('.header').removeClass('fixed');
     }
+});
+// Плавный скрол до секции
+$('a[href*="#"]').on('click', function (e) {
+    e.preventDefault();
+   
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 500,);
+  });
+
+// Анимация при достижении блока в зону видимости
+  $(document).ready(function() {
+
+	var windowHeight = $(window).height();
+ 
+	$(document).on('scroll', function() {
+		$('.anim_item').each(function() {
+			var self = $(this),
+			height = self.offset().top + self.height();
+			if ($(document).scrollTop() + windowHeight >= height) {
+				self.addClass('_animation')
+			}
+            else {
+				self.removeClass('_animation')
+            }
+		});
+	});
 });
