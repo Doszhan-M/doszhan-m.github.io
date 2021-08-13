2601fee45e257052ae40e8f568838ed5ea0bcfee
Doszhan <doszhan@mail.com>
2021-08-13 20:39:00 +0600

    add progress_bar

--- js/index.js
+++ js/index.js
@@ -1,51 +1,45 @@
-// Анимация при достижении блока в зону видимости
-$(document).ready(function () {
-
-  var windowHeight = $(window).height();
-
-  $(document).on('scroll', function () {
-    $('.anim_item').each(function () {
-      var self = $(this),
-        height = self.offset().top + self.height();
-      if ($(document).scrollTop() + windowHeight >= height) {
-        self.addClass('_animation')
-      }
-      else {
-        self.removeClass('_animation')
-      }
-    });
-  });
-
-
-  // Фиксация navbar после скрола
-  jQuery(window).scroll(function navBar() {
-    var the_top = jQuery(document).scrollTop();
-    if (the_top > 600) {
-      jQuery('.header').addClass('fixed');
-    }
-    else {
-      jQuery('.header').removeClass('fixed');
-    }
-  });
-
-  $(document).ready(function() {
-    var the_top = jQuery(document).scrollTop();
-    if (the_top > 600) {
-      jQuery('.header').addClass('fixed');
-    }
-    else {
-      jQuery('.header').removeClass('fixed');
-    }
-  });
-
-  
-  // Плавный скрол до секции
-  $('a[href*="#"]').on('click', function (e) {
-    e.preventDefault();
-
-    $('html, body').animate({
-      scrollTop: $($(this).attr('href')).offset().top
-    }, 500);
-  });
-
+// шаг вниз перед загрузкой и показать прелоадер
+$(window).ready(function() {
+  $("html, body").animate({ scrollTop: $(document).height() }, 100);
+  jQuery('#loading').fadeOut(3000);
 });
+
+
+// Анимация при достижении блока в зону видимости
+var windowHeight = $(window).height();
+
+$(document).on('scroll', function () {
+  $('.anim_item').each(function () {
+    var self = $(this),
+      height = self.offset().top + self.height();
+    if ($(document).scrollTop() + windowHeight >= height) {
+      self.addClass('_animation')
+    }
+    else {
+      self.removeClass('_animation')
+    }
+  });
+});
+
+
+// Фиксация navbar после скрола
+jQuery(window).scroll(function navBar() {
+  var the_top = jQuery(document).scrollTop();
+  if (the_top > 600) {
+    jQuery('.header').addClass('fixed');
+  }
+  else {
+    jQuery('.header').removeClass('fixed');
+  }
+});
+
+
+// Плавный скрол до секции
+$('a[href*="#"]').on('click', function (e) {
+  e.preventDefault();
+
+  $('html, body').animate({
+    scrollTop: $($(this).attr('href')).offset().top
+  }, 500);
+});
+
---
+++ js/progress_bar.js
@@ -0,0 +1,63 @@
+// заполнение програсса 
+function moveProgress(progress_elem_id, percent_elem_id, progress) {
+    var progress_elem = document.getElementById(progress_elem_id);
+    var percent_elem = document.getElementById(percent_elem_id);
+    var width = 100 - progress;
+    var id = setInterval(frame, 10);
+    console.log(1)
+    function frame() {
+        if (width >= progress) {
+            clearInterval(id);
+        } else {
+            width++;
+            progress_elem.style.width = width + '%';
+            percent_elem.innerHTML = width * 1 + '%';
+        }
+    }
+}
+
+// убывание програсса 
+function unMoveProgress(progress_elem_id, percent_elem_id, progress) {
+    var progress_elem = document.getElementById(progress_elem_id);
+    var percent_elem = document.getElementById(percent_elem_id);
+    var width = progress;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width <= 20) {
+            clearInterval(id);
+        } else {
+            width--;
+            progress_elem.style.width = width + '%';
+            percent_elem.innerHTML = width * 1 + '%';
+        }
+    }
+}
+
+// Заполнить или опустошить прогресс при достижении скрола 
+$(document).on('scroll', function () {
+
+    var windowHeight = $(window).height();
+
+    $('.progress_bar').each(function () {
+
+        var self = $(this);
+        const progress_elem_id = self.children().first().attr('id');
+        const percent_elem_id = self.children().filter(":nth-child(2)").attr('id');
+        const progress = self.children().last().text();
+        height = self.offset().top + self.height();
+        var the_top = jQuery(document).scrollTop();
+
+        if (!$(this).hasClass('not_progress') && the_top >= 800) {
+            // if ($(document).scrollTop() + windowHeight >= height) {
+                moveProgress(progress_elem_id, percent_elem_id, progress)
+                self.addClass('not_progress')
+            // }
+        }
+        else {
+            if ($(this).hasClass('not_progress') && the_top < 300) {
+                unMoveProgress(progress_elem_id, percent_elem_id, progress)
+                self.removeClass('not_progress')
+            }
+        }
+    });
+});
