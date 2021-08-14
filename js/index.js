9bf01c63dd52c99e7f17b46221f512ea2f70520a
Doszhan <doszhan@mail.com>
2021-08-15 01:39:39 +0600

    add gif for mobil bg

--- js/index.js
+++ js/index.js
@@ -27,7 +27,7 @@ $(document).on('scroll', function () {
 // Фиксация navbar после скрола
 jQuery(window).scroll(function navBar() {
   var the_top = jQuery(document).scrollTop();
-  if (the_top > 600) {
+  if (the_top > 500) {
     jQuery('.header').addClass('fixed');
   }
   else {
