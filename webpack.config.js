ae5c3aa4eceb983a3696bc54ac52d2f251f76398
Doszhan <doszhan.m@mail.ru>
2022-08-01 00:23:11 +0600

    add sitemap

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,22 @@
 const path = require('path');
+const SitemapPlugin = require('sitemap-webpack-plugin').default;
+// const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+
+const paths = [
+    {
+      path: '/',
+      lastmod: '2022-07-31',
+      priority: 1,
+      changefreq: 'yearly'
+    },
+    {
+      path: '/weather_forecast/',
+      lastmod: '2022-07-31',
+      priority: 0.8,
+      changefreq: 'never'
+    },
+  ];
 
 
 module.exports = {
@@ -7,5 +25,50 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'main.js'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use:["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|svg|mp4|ico|jpe?g|gif)$/,
+                include: /img/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                      name: '[name].[ext]',
+                      outputPath: 'images/',
+                      publicPath: 'images/'
+                    }
+                  }
+                ]
+              },
+        ]
+    },
+
+    plugins: [
+        // new HtmlWebpackPlugin (
+        //     {
+        //         template: "./index.html",
+        //         favicon: "./img/favicon.ico"
+        //     }
+        // ),
+        new SitemapPlugin({
+            base: 'https://doszhan-m.github.io/',
+            paths,
+            options: {
+              filename: 'sitemap.xml'
+            }
+          })
+    ]
 }
