f5f9b122eb1870ef9d1ed3a8cc2604ef9f68d661
Doszhan <doszhan.work@gmail.com>
2024-12-20 21:20:16 +0500

    feat:new webpack

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # My PortFolio
 https://doszhan-m.github.io/
+
+
+# Сжатие видео
+```bash
+sudo apt install ffmpeg -y
+ffmpeg -i input.mp4 -vcodec libx265 -crf 28 -preset veryslow output.mp4
+```
+- **-i input.mp4**: Указывает входной файл.
+
+- -**vcodec libx264**: Использует кодек H.264 для сжатия.
+
+- **-crf 23**: Уровень качества. Диапазон от 0 (без потерь) до 51 (наихудшее качество). Рекомендуется 18–28 (23 — хорошее компромиссное значение).
+
+- **-preset medium**: Скорость обработки. Варианты: ultrafast, superfast, fast, medium, slow, slower, veryslow. Чем медленнее, тем лучше качество при меньшем размере файла.
+
+- **output.mp4**: Имя выходного файла.
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,53 @@
 const path = require('path');
-const SitemapPlugin = require('sitemap-webpack-plugin').default;
-
-
-const paths = [
-    {
-      path: '/',
-      lastmod: '2025-01-10',
-      priority: 1,
-      changefreq: 'yearly'
-    },
-    {
-      path: '/weather_forecast/',
-      lastmod: '2025-01-22',
-      priority: 0.8,
-      changefreq: 'never'
-    },
-  ];
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 
 module.exports = {
-    devServer: {
-        static: {
-            directory: __dirname,
-        },
-        compress: true,
-        port: 8080,
-        open: true,
-        hot: true,
-    },
-    mode: 'production',
-    entry: './js/entry.js',
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'main.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.css$/,
-                use:["style-loader", "css-loader"]
-            },
-            {
-                test: /\.(png|svg|mp4|ico|jpe?g|gif)$/,
-                include: /img/,
-                use: [
-                  {
-                    loader: 'file-loader',
-                    options: {
-                      name: '[name].[ext]',
-                      outputPath: 'images/',
-                      publicPath: 'images/'
-                    }
-                  }
-                ]
-              },
-        ]
-    },
-
-    plugins: [
-        new SitemapPlugin({
-            base: 'https://doszhan-m.github.io/',
-            paths,
-            options: {
-              filename: 'sitemap.xml'
-            }
-          })
+  mode: 'development',
+  entry: './src/js/entry.js',
+  output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'), 
+  },
+  module: {
+      rules: [
+          {
+              test: /\.html$/,
+              use: ['html-loader']
+          },
+          {
+              test: /\.(png|svg|mp4|webm|ico|jpe?g|gif)$/,
+              type: 'asset/resource',
+              generator: {
+                  filename: 'img/[name][ext]'
+              }
+          },
+          {
+              test: /\.css$/,
+              type: 'asset/resource',
+              generator: {
+                  filename: 'css/[name][ext]'
+              }
+          }
     ]
-}
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+          template: './src/index.html'
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+            { from: './src/fonts', to: 'fonts' },
+            { from: './src/img', to: 'img' }
+        ]
+    })      
+  ],
+  devServer: {
+        port: 9000,
+        open: true
+    },
+  performance: {
+        maxAssetSize: 5000000
+    }
+};
